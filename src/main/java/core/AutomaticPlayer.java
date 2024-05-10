c4eca2cb9277cbd243c43e83c98e7c4ec2e411fa
Aniakrzacz <anna.krzaczkowska@student.uj.edu.pl>
2024-05-10 15:37:55 +0200

    core
    dodałam interface player

---
+++ src/main/java/core/AutomaticPlayer.java
@@ -0,0 +1,4 @@
+package core;
+
+public class AutomaticPlayer implements Player{
+}
---
+++ src/main/java/core/RealPlayer.java
@@ -0,0 +1,9 @@
+package core;
+
+import java.util.ArrayList;
+
+public class RealPlayer implements Player{
+    public static class Hand {
+        ArrayList<Card> myCards = new ArrayList<>();
+    }
+}
