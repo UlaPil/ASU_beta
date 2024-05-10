c1557078ed525cdf90e3ab971b93779730c78220
Szymon Trofimiec <sztrofimiec@gmail.com>
2024-05-10 15:35:38 +0200

    began the definition of class Card

--- src/main/java/core/Card.java
+++ src/main/java/core/Card.java
@@ -1,4 +1,27 @@
 package core;
 
-public class Card {
+public class Card implements Playable{
+    protected int number;
+    protected String color;
+    public Card(int number, String color) {
+        this.number = number;
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean isPlayable(String color, int number) {
+        if(this.color.equals(color) || this.number == number) {
+            return true;
+        }
+        if(color.equals("black")) {
+            return true;
+        }
+    }
 }
