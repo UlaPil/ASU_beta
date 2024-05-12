191e3246507870bb38641ece7d432e1782a377cf
Aniakrzacz <anna.krzaczkowska@student.uj.edu.pl>
2024-05-12 23:27:05 +0200

    play

--- src/main/java/viewModel/Play.java
+++ src/main/java/viewModel/Play.java
@@ -12,11 +12,18 @@ public class Play {
     public Board board;
     private static List<Playable> cards = new ArrayList<>();
     public Player currentPlayer;
+    private int modulo;
+    private int currentIndex;
+    private boolean draw;
     static {
         // inicjalizacja 64 kart
     }
 
     public Play(String ... players) {
+            // docelowo: modulo=players.length();
+        modulo = 4;     //na aktualne potrzeby
+        currentIndex = 0;
+        draw = false;
         board = new Board(cards);
         playerList = new ArrayList<>();
         Player player1 = new RealPlayer(players[0], board);
@@ -52,33 +59,48 @@ public class Play {
             throw new RuntimeException(e);
         }
         while(!play.gameOver()) {
-            if (play.currentPlayer.equals(play.playerList.get(0))) {
-                System.out.println("Now it's your turn. ");
-                System.out.println("Which card would you like to play? ");
-                System.out.println(play.currentPlayer); // wypisuje karty w ręce
-                System.out.print("Enter number or \"draw\" to draw a card from the pile: ");
-                String choice = scanner.nextLine();
-                // TODO: wykonać ruch gracza (on chyba nie ma metody i trzeba to zrobić manualnie)
-                System.out.println("You've finished your move. Now the top card is: ");
-                System.out.println(play.board.getTopCard());
-            } else {
-                System.out.println("Now it's " + play.currentPlayer.getName() + "'s turn...");
-                try {
-                    TimeUnit.SECONDS.sleep(5);
-                } catch (InterruptedException e) {
-                    continue;
+            try {
+                if (play.currentPlayer.equals(play.playerList.get(0))) {
+                    System.out.println("Now it's your turn. ");
+                    System.out.println("Which card would you like to play? ");
+                    System.out.println(play.currentPlayer); // wypisuje karty w ręce
+                    System.out.print("Enter number or \"draw\" to draw a card from the pile: ");
+                    String choice = scanner.nextLine();
+                    // TODO: wykonać ruch gracza (on chyba nie ma metody i trzeba to zrobić manualnie)
+                    System.out.println("You've finished your move. Now the top card is: ");
+                    System.out.println(play.board.getTopCard());
+                } else {
+                    System.out.println("Now it's " + play.currentPlayer.getName() + "'s turn...");
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+                        continue;
+                    }
+                    // TODO: wykonać ruch bota (on chyba ma do tego metode)
+                    System.out.println(play.currentPlayer.getName() + " has finished his move. Now the top card is: ");
+                    System.out.println(play.board.getTopCard());
                 }
-                // TODO: wykonać ruch bota (on chyba ma do tego metode)
-                System.out.println(play.currentPlayer.getName() + " has finished his move. Now the top card is: ");
-                System.out.println(play.board.getTopCard());
+                // TODO: ustawić current player na kolejnego z arraylisty playerów
+                play.currentIndex++;
+                play.currentIndex = play.currentIndex % play.modulo;
+                play.currentPlayer = play.playerList.get(play.currentIndex);
+                // TODO: UWAGA!!! ustalilismy że w play obsługujemy wyjatek ktory rzuva draw noMoreCards
+                //  wiec trzeba ko obsłużyć kończąc whila i wypisujac remis
+            } catch (NoMoreCardsInDeck n) {
+                play.draw=true;
+                break;
             }
-            // TODO: ustawić current player na kolejnego z arraylisty playerów
-
-            // TODO: UWAGA!!! ustalilismy że w play obsługujemy wyjatek ktory rzuva draw noMoreCards
-            //  wiec trzeba ko obsłużyć kończąc whila i wypisujac remis
         }
         // TODO: wypisać kto wygrał
-
+        if(play.draw) System.out.println("It is a draw! There is no winner.");
+        else {
+            for (Player player : play.playerList) {
+                if (player.didIWin()) {
+                    System.out.println("Congratulations! The winner is: " + player.getName() + ".");
+                    break;
+                }
+            }
+        }
         scanner.close();
     }
 }
