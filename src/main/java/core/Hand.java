9de8254a518664e750b15c415aa8c26d2ad54e85
UlaPil <ula.pilsniak@student.uj.edu.pl>
2024-05-13 13:49:22 +0200

    drobne poprawki

--- src/main/java/core/Hand.java
+++ src/main/java/core/Hand.java
@@ -4,23 +4,28 @@ import java.util.ArrayList;
 
 public class Hand {
     private ArrayList<Playable> cards = new ArrayList<>();
+
     @Override
     public String toString() {
         StringBuilder a = new StringBuilder();
         for(int i = 0 ; i < cards.size() ; i++) {
-            a.append(i + 1).append(':').append(cards.get(i)).append(", ");
+            a.append(i + 1).append(':').append(cards.get(i)).append("\n");
         }
         return a.toString();
     }
+
     public int getSize() {
         return cards.size();
     }
+
     public void putInHand(Playable card) {
         cards.add(card);
     }
+
     public Playable getFromHand(int index) {
         return cards.get(index);
     }
+
     public void removeFromHand(int index) {
         cards.remove(index);
     }
