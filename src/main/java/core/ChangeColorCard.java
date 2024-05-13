36311d45af8492219a956e63d1e95b22be169d1a
Aniakrzacz <anna.krzaczkowska@student.uj.edu.pl>
2024-05-13 17:38:06 +0200

    added some cards

--- src/main/java/core/ChangeColorCard.java
+++ src/main/java/core/ChangeColorCard.java
@@ -1,25 +1,20 @@
 package core;
 
 public class ChangeColorCard implements Playable{
-    
+    Card card;
+    public ChangeColorCard() { card = new Card("changecolor", "black") ; }
 
     @Override
-    public boolean isPlayable(String color, String symbol) {
-        return false;
-    }
+    public boolean isPlayable(String color, String symbol) { return true; }
 
     @Override
-    public String getColor() {
-        return ;
-    }
+    public String getColor() { return card.getColor(); }
 
     @Override
-    public String getSymbol() {
-        return null;
-    }
+    public String getSymbol() { return card.getSymbol(); }
 
     @Override
     public void onPlay(Board board) {
-
+        //board.setNextPlayerStatus("changecolor");
     }
 }
