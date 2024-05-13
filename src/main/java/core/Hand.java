791e107f209cf330ecdc5021f0896d55da1b057b
UlaPil <ula.pilsniak@student.uj.edu.pl>
2024-05-13 15:30:45 +0200

    dodalam sortowanie kart w rece po kolorze i symbolu

--- src/main/java/core/Hand.java
+++ src/main/java/core/Hand.java
@@ -1,12 +1,21 @@
 package core;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Hand {
     private ArrayList<Playable> cards = new ArrayList<>();
 
     @Override
     public String toString() {
+        cards.sort(new Comparator<Playable>() {
+            @Override
+            public int compare(Playable card1, Playable card2) {
+                int colorsCompare = card1.getColor().compareTo(card2.getColor());
+                if (colorsCompare != 0) return colorsCompare;
+                return card1.getSymbol().compareTo(card2.getSymbol());
+            }
+        });
         StringBuilder a = new StringBuilder();
         for(int i = 0 ; i < cards.size() ; i++) {
             a.append(i + 1).append(": ").append(cards.get(i)).append("\n");
