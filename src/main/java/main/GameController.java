07d8cd336e6691ef93ed1e5fd1c86bcffd45375b
UlaPil <ula.pilsniak@student.uj.edu.pl>
2024-05-26 13:28:38 +0200

    GameController class

---
+++ src/main/java/main/GameController.java
@@ -0,0 +1,13 @@
+package main;
+
+import javafx.event.ActionEvent;
+
+public class GameController {
+    public void ASU(ActionEvent e) {
+        System.out.println("ASU");
+    }
+
+    public void Draw(ActionEvent actionEvent) {
+        System.out.println("Draw");
+    }
+}
