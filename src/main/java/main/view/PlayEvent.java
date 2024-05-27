7aadfe51bb9edf27a8f0ebc093a8670eb59e87ff
UlaPil <ula.pilsniak@student.uj.edu.pl>
2024-05-27 23:10:23 +0200

    Teraz już chyba serio wszystko w GameView

---
+++ src/main/java/main/view/PlayEvent.java
@@ -0,0 +1,11 @@
+package main.view;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
+public class PlayEvent implements EventHandler<MouseEvent> {
+    @Override
+    public void handle(MouseEvent mouseEvent) {
+
+    }
+}
