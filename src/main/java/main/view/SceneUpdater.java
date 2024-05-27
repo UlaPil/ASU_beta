2e13614f18d98a9500c7a57c615b526c0157a2b1
Szymon Trofimiec <sztrofimiec@gmail.com>
2024-05-27 22:43:42 +0200

    ...

---
+++ src/main/java/main/view/SceneUpdater.java
@@ -0,0 +1,13 @@
+package main.view;
+
+import main.model.Game;
+
+public class SceneUpdater{
+    GameView gameView;
+    SceneUpdater(GameView gameView) {
+        this.gameView = gameView;
+    }
+    public void updateTopCard() {
+
+    }
+}
