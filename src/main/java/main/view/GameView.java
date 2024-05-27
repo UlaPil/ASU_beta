b9917cdcedeacdf09d072734d6929cef8ef67bb1
UlaPil <ula.pilsniak@student.uj.edu.pl>
2024-05-27 14:50:01 +0200

    Update GameView.java

--- src/main/java/main/view/GameView.java
+++ src/main/java/main/view/GameView.java
@@ -3,13 +3,31 @@ package main.view;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 
-public class GameView {
-    private static final int WIDTH = 1280;
-    private static final int HEIGHT = 720;
+public class GameView implements AsuScene{
     private final StackPane root = new StackPane();
 
-    public Scene getGameView() {
-
+    @Override
+    public Scene getScene() {
+        setBackground();
+        addDrawPile();
+        addTopCard();
+        addPlayerHand();
         return new Scene(root, WIDTH, HEIGHT);
+    }
+
+    private void setBackground() {
+
+    }
+
+    private void addDrawPile() {
+
+    }
+
+    private void addTopCard() {
+
+    }
+
+    private void addPlayerHand() {
+
     }
 }
