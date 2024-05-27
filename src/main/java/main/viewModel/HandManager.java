44434ef6a65edd269bb07f0c2fc017222a562911
Aniakrzacz <anna.krzaczkowska@student.uj.edu.pl>
2024-05-27 23:22:55 +0200

    HandObserver

--- src/main/java/main/viewModel/HandManager.java
+++ src/main/java/main/viewModel/HandManager.java
@@ -1,5 +1,20 @@
 package main.viewModel;
 
-public class HandManager {
+import main.model.Card;
+import main.model.Game;
+import main.model.Playable;
 
+public class HandManager {
+    Game game;
+
+    public HandManager(Game game) {
+        this.game = game;
+    }
+
+    public void updateAdd(Playable card) {
+
+    }
+    public void updateDelete(Playable card) {
+
+    }
 }
--- src/main/java/main/viewModel/HandObserver.java
+++ src/main/java/main/viewModel/HandObserver.java
@@ -1,12 +1,17 @@
 package main.viewModel;
 
+import main.model.Game;
 import main.model.Playable;
 
 public class HandObserver {
+    HandManager menager;
+    HandObserver(Game game) {
+        menager = new HandManager(game);
+    }
     public void updateAdd(Playable card) {
-
+        menager.updateAdd(card);
     }
     public void updateDelete(Playable card) {
-
+        menager.updateDelete(card);
     }
 }
