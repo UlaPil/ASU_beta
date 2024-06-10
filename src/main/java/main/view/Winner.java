cbefc4d07b7cb7fcea933d8ffde870547f50cede
Aniakrzacz <anna.krzaczkowska@student.uj.edu.pl>
2024-06-10 04:39:43 +0200

    Update Winner.java

--- src/main/java/main/view/Winner.java
+++ src/main/java/main/view/Winner.java
@@ -1,10 +1,12 @@
 package main.view;
+import javafx.application.Application;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
 
 public class Winner {
 
-    public static void show(int i) {
+    public static void show(Integer i) {
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle("Game Result");
         alert.getDialogPane().setStyle("-fx-font-family: 'Arial';");
@@ -13,9 +15,18 @@ public class Winner {
         if (i == 0) {
             alert.setContentText("Congratulations! You are the winner");
         } else {
-            alert.setContentText("I'm sorry, the winner is Bot" + i);
+            alert.setContentText("I'm sorry, the winner is Bot " + i.toString());
         }
 
         alert.showAndWait();
     }
+
+//    public static class DummyApp extends Application {
+//        @Override
+//        public void start(Stage primaryStage) {
+//            Winner.show(1);
+//            primaryStage.close();
+//        }
+//    }
+
 }
