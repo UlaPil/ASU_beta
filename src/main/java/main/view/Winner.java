3bd8cab79ad9b0a313c14f47c5b0692fdce2eebf
UlaPil <ula.pilsniak@student.uj.edu.pl>
2024-06-16 13:14:25 +0200

    .

--- src/main/java/main/view/Winner.java
+++ src/main/java/main/view/Winner.java
@@ -5,57 +5,69 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
 import javafx.stage.StageStyle;
 
+import java.util.Objects;
+
 public class Winner {
 
-    public void show(Integer i) {
+    public static void show(Integer i) {
         Stage window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
-        window.initStyle(StageStyle.UNDECORATED);
         window.setHeight(300);
         window.setWidth(370);
+
         Label label = new Label();
         label.setStyle("-fx-font-family: 'Arial'; -fx-font-size: 16px;");
+
         ImageView imageView;
         if (i == 0) {
             label.setText("Congratulations! You are the winner");
             imageView = new ImageView("winner.png");
         } else {
             label.setText("I'm sorry, the winner is Bot " + i.toString());
-            imageView = new ImageView("loser.jpg");
+            imageView = new ImageView("loser.png");
         }
         imageView.setFitHeight(150);
         imageView.setPreserveRatio(true);
+
         Button closeButton = new Button("OK");
         closeButton.setStyle("-fx-font-family: 'Arial'; -fx-font-size: 14px;");
         closeButton.setOnAction(e -> window.close());
+
         VBox layout = new VBox(10);
         layout.getChildren().addAll(label, imageView, closeButton);
         layout.setAlignment(Pos.CENTER);
-        layout.setStyle(
-                "-fx-background-color: white; " +
-                "-fx-background-radius: 20px; " +
-                "-fx-padding: 20px;"
-        );
-        Scene scene = new Scene(layout, 300, 370);
+
+        StackPane root = new StackPane(layout);
+        Rectangle rectangle = new Rectangle(370, 300);
+        rectangle.setArcHeight(60.0);
+        rectangle.setArcWidth(60.0);
+        root.setClip(rectangle);
+
+        Scene scene = new Scene(root, 370, 300);
+        scene.setFill(Color.TRANSPARENT);
+        window.initStyle(StageStyle.TRANSPARENT);
         window.setScene(scene);
         window.showAndWait();
     }
 
 
 
-//    public static class DummyApp extends Application {
-//        @Override
-//        public void start(Stage primaryStage) {
-//            Winner.show(0);
-//            primaryStage.close();
-//        }
-//    }
+    public static class DummyApp extends Application {
+        @Override
+        public void start(Stage primaryStage) {
+            Winner.show(0);
+            primaryStage.close();
+        }
+    }
 
 }
