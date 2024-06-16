f2ff3ce2d2e5b138e10ca886160525efd7dd0c28
UlaPil <ula.pilsniak@student.uj.edu.pl>
2024-06-16 23:55:07 +0200

    .

--- src/main/java/main/view/AppInit.java
+++ src/main/java/main/view/AppInit.java
@@ -46,7 +46,7 @@ public class AppInit {
         Menu scene = (Menu)Scenes.get(SceneName.MENU);
         scene.setEvent(Menu.But.EXIT, getCloser());
         scene.setEvent(Menu.But.PLAY, getGameStarter());
-        scene.setEvent(Menu.But.HISTORY, getSceneChanger(SceneName.HISTORY));
+        scene.setEvent(Menu.But.HISTORY, getHistoryOpener());
         //GameView
         GameView scene2 = (GameView)Scenes.get(SceneName.PLAY);
         scene2.defineExit(getCloser());
