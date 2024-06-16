1830acce046c73b66946efe505c7bb22662e7d2a
UlaPil <ula.pilsniak@student.uj.edu.pl>
2024-06-16 12:37:57 +0200

    EndGameWindow szybki design

--- src/main/java/main/view/Winner.java
+++ src/main/java/main/view/Winner.java
@@ -1,30 +1,59 @@
 package main.view;
+
 import javafx.application.Application;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.stage.StageStyle;
 
 public class Winner {
 
     public void show(Integer i) {
-        Alert alert = new Alert(AlertType.INFORMATION);
-        alert.setTitle("Game Result");
-        alert.getDialogPane().setStyle("-fx-font-family: 'Arial';");
-        alert.setHeaderText(null);
-
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.initStyle(StageStyle.UNDECORATED);
+        window.setHeight(300);
+        window.setWidth(370);
+        Label label = new Label();
+        label.setStyle("-fx-font-family: 'Arial'; -fx-font-size: 16px;");
+        ImageView imageView;
         if (i == 0) {
-            alert.setContentText("Congratulations! You are the winner");
+            label.setText("Congratulations! You are the winner");
+            imageView = new ImageView("winner.png");
         } else {
-            alert.setContentText("I'm sorry, the winner is Bot " + i.toString());
+            label.setText("I'm sorry, the winner is Bot " + i.toString());
+            imageView = new ImageView("loser.jpg");
         }
-
-        alert.showAndWait();
+        imageView.setFitHeight(150);
+        imageView.setPreserveRatio(true);
+        Button closeButton = new Button("OK");
+        closeButton.setStyle("-fx-font-family: 'Arial'; -fx-font-size: 14px;");
+        closeButton.setOnAction(e -> window.close());
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, imageView, closeButton);
+        layout.setAlignment(Pos.CENTER);
+        layout.setStyle(
+                "-fx-background-color: white; " +
+                "-fx-background-radius: 20px; " +
+                "-fx-padding: 20px;"
+        );
+        Scene scene = new Scene(layout, 300, 370);
+        window.setScene(scene);
+        window.showAndWait();
     }
+
+
 
 //    public static class DummyApp extends Application {
 //        @Override
 //        public void start(Stage primaryStage) {
-//            Winner.show(1);
+//            Winner.show(0);
 //            primaryStage.close();
 //        }
 //    }
